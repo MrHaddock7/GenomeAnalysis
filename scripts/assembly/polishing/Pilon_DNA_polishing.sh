d1d1e833695d0957b9fe60e6ba3784bad094093e
MrHaddock7 <113512371+MrHaddock7@users.noreply.github.com>
2025-04-17 21:56:28 +0200

    Changed the mapping script so it aligns the unpaired trimmomatic outputs and fixed the pilon script for the dna

--- scripts/assembly/polishing/Pilon_DNA_polishing.sh
+++ scripts/assembly/polishing/Pilon_DNA_polishing.sh
@@ -4,14 +4,28 @@
 #SBATCH -p core
 #SBATCH -n 15
 #SBATCH -t 45:00
-#SBATCH -J DNA_Mapping
+#SBATCH -J DNA_Polishing
 #SBATCH --output=%x.%j.out
 
 BAM_OUTPUT='/home/haddock/private/Genome_analysis/GenomeAnalysis/data/mapped_outputs/'
 FLYE_INPUT='/home/haddock/private/Genome_analysis/GenomeAnalysis/analyses/01_preprocessing/09-04-Flye_dna_assembly/'
-ILLUMINA_INPUT='/home/haddock/private/Genome_analysis/GenomeAnalysis/analyses/01_preprocessing/02-04-trimomatic_dna/'
 
 # Load modules
 module load bioinfo-tools
 module load Pilon
 
+for i in DV3 HP126 R7;
+do (
+java -jar "${PILON_HOME}/pilon.jar" \
+ --threads 5 \
+ --genome "${FLYE_INPUT}${i}/${i}_assembly_flye.fasta" \
+ --frags "${BAM_OUTPUT}${i}.paired.sorted.bam" \
+ --unpaired "${BAM_OUTPUT}${i}.unpaired.sorted.bam" \
+ --outdir "${BAM_OUTPUT}" \
+ --output "${i}_pilon" \
+ --changes \
+ --vcf
+) &
+done
+
+wait
